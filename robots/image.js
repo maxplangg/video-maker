c4847b377835ce8ffe5f56c55cae5e8854dfba90
Max Plangg <pcf.maximilian@gmail.com>
2020-04-14 20:59:23 -0300

    feat: add image donwload logic with fallback

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require('./state.js')
@@ -8,6 +9,7 @@ async function robot() {
     const content = state.load()
 
     await fechImagesOfAllSentences(content)
+    await downloadAllImages(content)
 
     state.save(content)
 
@@ -23,7 +25,7 @@ async function robot() {
       
             content.sentences[sentenceIndex].images = await fetchGoogleAndReturnImagesLinks(query)
             content.sentences[sentenceIndex].googleSearchQuery = query
-          }
+        }
     }
 
     async function fetchGoogleAndReturnImagesLinks(query) {
@@ -39,7 +41,40 @@ async function robot() {
             return item.link
         })
 
-        return imagesUrl
+        return imagesUrl 
+    }
+
+    async function downloadAllImages(content) {
+        content.downloadedImages = []
+
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            const images = content.sentences[sentenceIndex].images
+
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+                const imageUrl = images[imageIndex]
+
+                try {
+                    if (content.downloadedImages.includes(imageUrl))
+                        throw new Error("Imagem já foi baixada")
+                    
+                    await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`)
+
+                    console.log(`> Baixou imagem com sucesso: ${imageUrl}`)
+
+                    content.downloadedImages.push(imageUrl)
+                    break
+                } catch (error) {
+                    console.log(`> Erro ao baixar ${imageUrl}: ${error}`)
+                }
+            }
+        }
+    }
+
+    async function downloadAndSave(url, fileName) {
+        return imageDownloader.image({
+            url: url,
+            dest: `./content/${fileName}`
+        })
     }
 }
 
